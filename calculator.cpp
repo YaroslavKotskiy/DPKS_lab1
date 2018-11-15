90caaffc9003ca9f7bf0d7999a6ff26d2d02958c
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:17:29 +0200

    add a subtraction operation
    Add member-function which performs a subtraction in terms of
    an addition.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>
    
    fix truncation error
    
    To convert float to integer the truncation is performed, but the
    rounding is expected.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>
    Signed-off-by: Yaroslav Klymenko <yaroslav.klymenko98@gmail.com>

--- calculator.cpp
+++ calculator.cpp
@@ -2,5 +2,18 @@
 
 int Calculator::Add (int a, int b)
 {
+<<<<<<< HEAD
+<<<<<<< HEAD
     return a + b;
+=======
+	return a + b + 0.5;
+>>>>>>> 4ad40a3... fix truncation error
+=======
+	return a + b;
+>>>>>>> cbcb06d... formatting: use tabs instead of spaces
+}
+
+int Calculator::Sub (int a, int b)
+{
+	return Add (a, -b);
 }
--- calculator.h
+++ calculator.h
@@ -3,8 +3,15 @@
 
 class Calculator
 {
+<<<<<<< HEAD
     public:
         int Add (int, int);
+        int Sub (int, int);
+=======
+	public:
+		int Add (double, double);
+		int Sub (double, double);
+>>>>>>> 7b4b3c8... formatting: use tabs instead of spaces
 };
 
 #endif//CALCULATOR_H
